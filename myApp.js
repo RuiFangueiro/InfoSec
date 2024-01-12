3953c39fabf37b8b0dc609532e1d36485ba8d937
Replit user <>
2024-01-12 19:24:23 +0000

    helmet version

--- myApp.js
+++ myApp.js
@@ -18,7 +18,7 @@ app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
-//app.disable("strict-transport-security");
+app.disable("strict-transport-security");
 app.use("/_api", api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
