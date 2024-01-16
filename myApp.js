6b38b07604ef35ef63fd81d560c7460575853f0e
Replit user <>
2024-01-16 18:25:40 +0000

    maxage 1

--- myApp.js
+++ myApp.js
@@ -3,15 +3,13 @@ const helmet = require("helmet");
 const app = express();
 const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
-app.disable("strict-transport-security");
-
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+app.use(helmet.hsts({ maxAge: 1, force: true }));
 app.use(helmet.dnsPrefetchControl({ allow: false }));
 
 
@@ -20,7 +18,7 @@ app.use(helmet.dnsPrefetchControl({ allow: false }));
 module.exports = app;
 const api = require("./server.js");
 app.use(express.static("public"));
-
+app.disable("strict-transport-security");
 app.use("/_api", api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
